dc63ce75e127403e5f5426f28d52d3f45a46fddd
Jan Kristanto <jan.nctu@gmail.com>
2019-01-05 01:50:17 +0800

    implement rotation translation noise

--- data_aug.py
+++ data_aug.py
@@ -1,27 +1,44 @@
 import tensorflow as tf
 import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+from scipy.misc import imread
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
-# referenced
-def tf_resize_images(X_img_file_paths,IMAGE_SIZE,CH):
+def load_images(X_img_file_paths):
+    images = []
+    for index, file_path in enumerate(X_img_file_paths):
+        im = mpimg.imread(file_path)  # Do not read alpha channel.
+        #im = imread(file_path)
+        #print(len(im.shape))
+        #quit()
+        if len(im.shape) < 3:
+            images.append(np.expand_dims(np.expand_dims(im, 2), 0))
+        else:
+            images.append(np.expand_dims(im, 0))
+
+
+    images = np.concatenate(images)
+    #sz = images.shape
+    #if len(sz) < 4:
+    #X_data = np.reshape(images, (sz[0],sz[1],sz[2],1))
+    #else:
+
+    return images
+
+def tf_resize_images(X_imgs,IMAGE_SIZE,CH):
     X_data = []
     tf.reset_default_graph()
     X = tf.placeholder(tf.float32, (None, None, CH))
     tf_img = tf.image.resize_images(X, [IMAGE_SIZE, IMAGE_SIZE], tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-
-        # Each image is resized individually as different image may be of different size.
-        for index, file_path in enumerate(X_img_file_paths):
-            img = mpimg.imread(file_path)[:, :, :CH]  # Do not read alpha channel.
+        for img in X_imgs:
             resized_img = sess.run(tf_img, feed_dict={X: img})
             X_data.append(resized_img)
-
     X_data = np.array(X_data, dtype=np.float32)  # Convert to numpy
     return X_data
-
 
 def central_scale_images(X_imgs, scales,IMAGE_SIZE,CH):
     # Various settings needed for Tensorflow operation
@@ -66,3 +83,110 @@ def flip_images(X_imgs,IMAGE_SIZE,CH):
     return X_flip
 
 
+from math import ceil, floor
+
+def get_translate_parameters(index,IMAGE_SIZE):
+    if index == 0:  # Translate left 20 percent
+        offset = np.array([0.0, 0.2], dtype=np.float32)
+        size = np.array([IMAGE_SIZE, ceil(0.8 * IMAGE_SIZE)], dtype=np.int32)
+        w_start = 0
+        w_end = int(ceil(0.8 * IMAGE_SIZE))
+        h_start = 0
+        h_end = IMAGE_SIZE
+    elif index == 1:  # Translate right 20 percent
+        offset = np.array([0.0, -0.2], dtype=np.float32)
+        size = np.array([IMAGE_SIZE, ceil(0.8 * IMAGE_SIZE)], dtype=np.int32)
+        w_start = int(floor((1 - 0.8) * IMAGE_SIZE))
+        w_end = IMAGE_SIZE
+        h_start = 0
+        h_end = IMAGE_SIZE
+    elif index == 2:  # Translate top 20 percent
+        offset = np.array([0.2, 0.0], dtype=np.float32)
+        size = np.array([ceil(0.8 * IMAGE_SIZE), IMAGE_SIZE], dtype=np.int32)
+        w_start = 0
+        w_end = IMAGE_SIZE
+        h_start = 0
+        h_end = int(ceil(0.8 * IMAGE_SIZE))
+    else:  # Translate bottom 20 percent
+        offset = np.array([-0.2, 0.0], dtype=np.float32)
+        size = np.array([ceil(0.8 * IMAGE_SIZE), IMAGE_SIZE], dtype=np.int32)
+        w_start = 0
+        w_end = IMAGE_SIZE
+        h_start = int(floor((1 - 0.8) * IMAGE_SIZE))
+        h_end = IMAGE_SIZE
+
+    return offset, size, w_start, w_end, h_start, h_end
+
+def translate_images(X_imgs,IMAGE_SIZE,CH):
+    offsets = np.zeros((len(X_imgs), 2), dtype=np.float32)
+    n_translations = 4
+    X_translated_arr = []
+
+    tf.reset_default_graph()
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for i in range(n_translations):
+            X_translated = np.zeros((len(X_imgs), IMAGE_SIZE, IMAGE_SIZE, CH),
+                                    dtype=np.float32)
+            X_translated.fill(1.0)  # Filling background color
+            base_offset, size, w_start, w_end, h_start, h_end = get_translate_parameters(i,IMAGE_SIZE)
+            offsets[:, :] = base_offset
+            glimpses = tf.image.extract_glimpse(X_imgs, size, offsets)
+
+            glimpses = sess.run(glimpses)
+            X_translated[:, h_start: h_start + size[0], \
+            w_start: w_start + size[1], :] = glimpses
+            X_translated_arr.extend(X_translated)
+    X_translated_arr = np.array(X_translated_arr, dtype=np.float32)
+    return X_translated_arr
+
+def rotate_images(X_imgs, IMAGE_SIZE,CH):
+    X_rotate = []
+    tf.reset_default_graph()
+    X = tf.placeholder(tf.float32, shape=(IMAGE_SIZE, IMAGE_SIZE, CH))
+    k = tf.placeholder(tf.int32)
+    tf_img = tf.image.rot90(X, k=k)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for img in X_imgs:
+            for i in range(3):  # Rotation at 90, 180 and 270 degrees
+                rotated_img = sess.run(tf_img, feed_dict={X: img, k: i + 1})
+                X_rotate.append(rotated_img)
+
+    X_rotate = np.array(X_rotate, dtype=np.float32)
+    return X_rotate
+
+
+def add_salt_pepper_noise(X_imgs,salt_vs_pepper,amount):
+    # Need to produce a copy as to not modify the original image
+    X_imgs_copy = X_imgs.copy()
+    row, col, _ = X_imgs_copy[0].shape
+    num_salt = np.ceil(amount * X_imgs_copy[0].size * salt_vs_pepper)
+    num_pepper = np.ceil(amount * X_imgs_copy[0].size * (1.0 - salt_vs_pepper))
+
+    for X_img in X_imgs_copy:
+        # Add Salt noise
+        coords = [np.random.randint(0, i - 1, int(num_salt)) for i in X_img.shape]
+        X_img[coords[0], coords[1], :] = 1
+
+        # Add Pepper noise
+        coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in X_img.shape]
+        X_img[coords[0], coords[1], :] = 0
+    return X_imgs_copy
+
+
+import cv2
+
+def add_gaussian_noise(X_imgs):
+    gaussian_noise_imgs = []
+    row, col, ch = X_imgs[0].shape
+
+    #sigma = var ** 0.5
+    for X_img in X_imgs:
+        gaussian = np.random.random((row, col, 1)).astype(np.float32)
+        #gaussian = np.random.normal(mean, sigma, (row, col, ch)).astype(np.float32)
+        gaussian = np.concatenate((gaussian, gaussian, gaussian), axis=2)
+        gaussian_img = cv2.addWeighted(X_img, 0.75, 0.25 * gaussian, 0.25, 0)
+        gaussian_noise_imgs.append(gaussian_img)
+    gaussian_noise_imgs = np.array(gaussian_noise_imgs, dtype=np.float32)
+    return gaussian_noise_imgs
--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import tensorflow as tf
-import matplotlib.image as mpimg
+#import matplotlib.image as mpimg
+from scipy.misc import imread
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
-from data_aug import tf_resize_images, central_scale_images,flip_images
+from data_aug import *
 import cv2
 # create image paths list
 IMAGE_SIZE = 256
 CH = 3
 image_paths = []
+#image_paths.append('sample/normal/img_5001.png')
 image_paths.append('sample/normal/000001.png')
-
+images = load_images(image_paths)
+#plt.imshow(images[0])
+#plt.show()
+#quit()
 # store returned list of numpy arrays
-resize_imgs = tf_resize_images(image_paths,IMAGE_SIZE,CH)
+resize_imgs = tf_resize_images(images,IMAGE_SIZE,CH)
 scaled_imgs = central_scale_images(resize_imgs, [0.90, 0.75, 0.60],IMAGE_SIZE,CH)
 flipped_images = flip_images(resize_imgs,IMAGE_SIZE,CH)
-
+translated_imgs = translate_images(resize_imgs,IMAGE_SIZE,CH)
+rotated_imgs = rotate_images(resize_imgs,IMAGE_SIZE,CH)
+salt_pepper_noise_imgs = add_salt_pepper_noise(resize_imgs,0.2,0.004)
+gaussian_noise_imgs = add_gaussian_noise(resize_imgs)
 # iterate through the array, and save the image to the directory
 # data is array of numpy arrays
 # so data[i] used to write, not data
-for i in range(len(resize_imgs)):
+for i in range(len(gaussian_noise_imgs)):
     #img = cv2.cvtColor(data[i], cv2.COLOR_RGB2BGR)
-    plt.imshow(flipped_images[i])
+    plt.imshow(gaussian_noise_imgs[i])
+    #print(flipped_images[i].shape)
     plt.show()
     #cv2.imwrite("image_output" + str(i) + ".jpg", img)
     # unable to show the image output properly - to-do
